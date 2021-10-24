8707c3c9e659372951e1be411cf611ad58d5df5f
WingLim <643089849@qq.com>
2021-10-25 07:44:10 +0800

    refactor(hook): remove nothing case
    Only success and fail will triger webhook in ddns-go

--- api/hook.go
+++ api/hook.go
@@ -19,8 +19,8 @@ type IP struct {
 }
 
 type DDNSRequest struct {
-	IPv4 IP `json:"ipv4,omitempty"`
-	IPv6 IP `json:"ipv6,omitempty"`
+	IPv4 *IP `json:"ipv4,omitempty"`
+	IPv6 *IP `json:"ipv6,omitempty"`
 }
 
 type HookResponse struct {
@@ -34,7 +34,7 @@ func HookHandler(w http.ResponseWriter, r *http.Request) {
 
 	var req DDNSRequest
 	if err := json.Unmarshal(body, &req); err != nil {
-		log.Fatal("Error to parse DDNS request", err)
+		log.Fatal("Error to parse DDNS request: ", err)
 	}
 
 	token := os.Getenv("TOKEN")
@@ -47,33 +47,27 @@ func HookHandler(w http.ResponseWriter, r *http.Request) {
 
 	text := ""
 
-	if req.IPv4.Result != "未改变" {
+	if req.IPv4 != nil {
 		text += fmt.Sprintf("IPv4: %s\n%s\n%s\n", req.IPv4.Result, req.IPv4.Addr, req.IPv4.Domain)
 	}
 
-	if req.IPv6.Result != "未改变" {
+	if req.IPv6 != nil {
 		text += fmt.Sprintf("IPv6: %s\n%s\n%s\n", req.IPv4.Result, req.IPv6.Addr, req.IPv6.Domain)
 	}
 
-	w.Header().Add("Content-Type", "application/json")
 	var resp HookResponse
-	if text != "" {
-		msg := tgbotapi.NewMessage(chatId, text)
-		if _, err = bot.Send(msg); err != nil {
-			resp = HookResponse{
-				Status: false,
-				Msg:    err.Error(),
-			}
-		} else {
-			resp = HookResponse{
-				Status: true,
-			}
+	msg := tgbotapi.NewMessage(chatId, text)
+	if _, err = bot.Send(msg); err != nil {
+		resp = HookResponse{
+			Status: false,
+			Msg:    err.Error(),
 		}
 	} else {
 		resp = HookResponse{
 			Status: true,
-			Msg:    "IPv4 and IPv6 didn't change",
 		}
 	}
+
+	w.Header().Add("Content-Type", "application/json")
 	_ = json.NewEncoder(w).Encode(resp)
 }

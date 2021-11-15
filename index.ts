ff0f8612d88688babb6d041027685af71b8ab849
WingLim <643089849@qq.com>
2021-11-15 19:53:47 +0800

    fix: use BOT_URL

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import { Bot } from 'grammy'
 
-const { BOT_TOKEN, BOT_WEBHOOK } = process.env
+const { BOT_TOKEN, BOT_URL } = process.env
 
 const bot = new Bot(BOT_TOKEN)
 
 console.log('Setting Webhook')
-let res = await bot.api.setWebhook(BOT_WEBHOOK)
+let res = await bot.api.setWebhook(`${BOT_URL}/api/bot`)
 console.log(res)
